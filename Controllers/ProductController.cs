366b11459d41c57030334c4c113b1250510d7391
Brad Weir <bradrweir@gmail.com>
2022-02-15 14:52:04 -0500

    get by Context

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -7,21 +7,20 @@ namespace template_csharp_reviews_site.Controllers
 {
     public class ProductController : Controller
     {
-        List<Product> cellPhones = new ApplicationContext().Products.ToList();
+        private ApplicationContext _context;
 
         public ProductController()
         {
-            
+            _context = new ApplicationContext();
         }
         public IActionResult Index()
         {
-
-            return View(cellPhones);
+            return View(_context.Products.ToList());
         }
 
         public IActionResult Detail(int id)
         {
-            Product cellPhone = cellPhones.Find(cellPhones => cellPhones.Id == id);
+            Product cellPhone = _context.Products.ToList().Find(cellPhone => cellPhone.Id == id);
             return View(cellPhone);
         }
     }
