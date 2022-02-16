8bfb1659ffddf5550323c9ab055df5bcf3e5cd8f
Brad Weir <bradrweir@gmail.com>
2022-02-16 17:36:02 -0500

    Added Product CRUD, TagHelpers, Edit Delete in Details, Create in index

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -23,5 +23,38 @@ namespace template_csharp_reviews_site.Controllers
             Product cellPhone = _context.Products.Find(id);
             return View(cellPhone);
         }
+
+        public IActionResult Add()
+        {
+            return View(new Product() { ReleaseDate = System.DateTime.Now });
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(int id)
+        {
+            return View(_context.Products.Find(id));
+        }
+        [HttpPost]
+        public IActionResult Update(Product product)
+        {
+            _context.Products.Update(product);
+            _context.SaveChanges();
+            return RedirectToAction("Detail", new { Id = product.Id });
+        }
+
+        public IActionResult Remove(int id)
+        {
+            _context.Products.Remove(_context.Products.Find(id));
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
