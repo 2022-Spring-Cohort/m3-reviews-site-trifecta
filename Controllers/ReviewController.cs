313e8a89aa832dddbd138490c3098e0b21b8f954
RimmaGirsh <rimma.girsheva@gmail.com>
2022-02-18 13:19:23 -0800

    AddandEdit

--- Controllers/ReviewController.cs
+++ Controllers/ReviewController.cs
@@ -25,5 +25,47 @@ namespace template_csharp_reviews_site.Controllers
             ViewBag.cellPhone = _context.Products.Find(review.ProductId);
             return View(review);
         }
+
+        public IActionResult Add(int id)
+        {
+
+            return View(new Review());
+
+        }
+
+         [HttpPost]
+        public IActionResult Add(Review review)
+        {
+            review.ReviewDate=System.DateTime.Now;
+            _context.Reviews.Add(review);
+            _context.SaveChanges();
+
+            return RedirectToAction("Detail","Product",new { id = review.ProductId});
+
+        }
+
+
+        public IActionResult Edit(int id)
+        {
+            Review selectReview = _context.Reviews.Find(id);
+            return View(selectReview);
+
+
+        }
+
+       [HttpPost]
+        public IActionResult Edit(int id,Review review)
+        {
+            _context.Reviews.Update(review);
+            _context.SaveChanges();
+
+            return RedirectToAction("Detail","Product",new {id = review.ProductId});
+        }
+
+        
+
+
     }
+   
+
 }
